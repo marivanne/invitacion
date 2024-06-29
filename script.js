6e55ccde97278373942b71f296b14d19d54995dc
marivanne <156456878+marivanne@users.noreply.github.com>
2024-06-29 14:24:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const card = document.querySelector('.card');
+const texto = document.querySelector('.texto');
 
 card.addEventListener('click', () => {
   card.classList.toggle('is-open');
+  texto.classList.toggle('is-open');
 });
